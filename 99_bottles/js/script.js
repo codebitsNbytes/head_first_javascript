420e6fe0bb99815a8e2a1421917113b2fd7441d6
codebitsNbytes <147984047+codebitsNbytes@users.noreply.github.com>
2024-07-25 03:18:25 +0200

    battle ship game v1

---
+++ 99_bottles/js/script.js
@@ -0,0 +1,57 @@
+
+//
+let age = 15;
+let userName = "Owen";
+
+if (age > 14) {
+  alert ("Sorry this page is for kids only!");
+} else {
+  alert ("Welcome " + userName + "!");
+}
+
+let scoops = 5;
+while (scoops > 0) {
+  console.log("Another scoop!<br>" + scoops);
+  scoops = scoops - 1;
+}
+
+console.log("Life without ice cream isn't the same");
+
+//
+console.log(`**********************************`);
+
+let birthdayPerson = "Joe";
+let i = 0;
+
+while (i < 2) {
+  console.log("Happy Birthday to you." + i);
+  i = i + 1;
+}
+
+console.log("Happy Birthday dear " + birthdayPerson + ",");
+console.log("Happy Birthday to you.");
+
+console.log(`**********************************`);
+//
+
+let word = "bottles";
+let singleWord = "bottle";
+let count = 99;
+let chorus = "Take one down pass it around";
+
+while (count >= 0) {
+  if (count > 1) {
+    console.log(`${count} ${word} of beer on the wall`);
+    console.log(`${count} ${word} of beer`);
+    console.log(`${chorus}`);
+  }
+  else if (count === 1) {
+    console.log(`${count} ${singleWord} of beer on the wall`);
+    console.log(`${count} ${singleWord} of beer`);
+    console.log(`${chorus}`);
+  }
+  else {
+    console.log(`No more ${word} of beer on the wall`);
+  }
+  count = count - 1;
+}
