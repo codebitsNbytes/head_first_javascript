157836ad192d6d9479c4797229d306ebe2b644a9
codebitsNbytes <147984047+codebitsNbytes@users.noreply.github.com>
2024-08-31 02:23:06 +0200

    chapter 6 first commit

---
+++ chapter_6/js/script.js
@@ -0,0 +1,11 @@
+// page 231
+var access = document.getElementById("code9");
+var code = access.innerHTML;
+
+code = `${code} midnight`;
+alert (code);
+
+// page 242
+var planet = document.getElementById("greenplanet");
+planet.innerHTML = "Red Alert: hit by phaser fire!";
+console.log(planet.innerHTML);
